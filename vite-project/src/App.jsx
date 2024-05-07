8aa423520536160d1e093dc2a5b89f645fb14538
hajar <hajar.dahnel@gmail.com>
2024-05-07 21:40:49 +0800

    day-2

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,33 +1,11 @@
-import Title from "./components/Title";
-import Cat from "./components/Cat";
-
+import Card from "./components/Card";
 
 function App() {
-  const familyName = 'AlBukerrr';
+  
   return (
-    <div style={{
-      display: 'flex',
-      flexDirection: 'column',
-      gap: '1rem',
-      padding: '1rem',
-      }}>
-
-
-      <h1 style={{ fontSize: 50 }}>React App</h1>
-      <p>Hello World</p>
-      <Title 
-        name = {`Hajar ${familyName}`}
-        //role = "Developer"
-        isManager={true}
-        // meow = {() => console.log("Meow!")}
-      />
-      <Title 
-        name = {`Fauzan ${familyName}`}
-        role = "CEO"
-      />
- 
-    </div>
-    
+   <>
+    <Card name = "Hajar" />
+   </>
 
   )
 
---
+++ vite-project/src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Card = (props) => {
+  return (
+    <div>
+        {props.name}
+        <p>Hello</p>
+    </div>
+  )
+}
+
+Card.propTypes = {
+    name: PropTypes.string,
+}
+
+export default Card
